1450cbe4ce95e244044b292b73c55cadd23e0d02
Luke Pacholski <luke.pacholski@gmail.com>
2016-03-04 11:38:07 -0800

    Etherpad test

---
+++ template-types/javascript/editor-etherpad.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+
+
+});
+
+function newContent(content){
+  $(".page").html(content);
+  var pads = $(".page .etherpad");
+  pads.each(function(i,el){
+    var url = $(el).text();
+
+    var iframe = $("<iframe class='etherpad-embed' />");
+    iframe.attr("src",url);
+    $(el).replaceWith(iframe);
+
+  });
+}
+
+tinymce.init({
+  setup : function(ed) {
+    ed.on('init', function(e) {
+       newContent(ed.getContent());
+    });
+
+    ed.on('keyup', function(e) {
+       newContent(ed.getContent());
+    });
+    ed.on('change', function(e) {
+       newContent(ed.getContent());
+    });
+
+  },
+  selector: 'textarea',
+  height: 800,
+  menubar : 'insert',
+  toolbar: 'undo redo | template | styleselect | bold italic | link image | alignleft aligncenter alignright',
+  plugins: [
+     'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+     'searchreplace wordcount visualblocks visualchars code fullscreen',
+     'template insertdatetime media nonbreaking save table contextmenu directionality',
+     'emoticons template paste textcolor colorpicker textpattern imagetools'
+   ],
+  menu : {},
+  formats: {
+    alignleft: { selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: 'left' },
+    aligncenter: { selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: 'center' },
+    alignright: { selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: 'right' },
+    alignfull: { selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes: 'full' },
+    bold: { inline: 'span', 'classes': 'bold' },
+    italic: { inline: 'span', 'classes': 'italic' },
+    underline: { inline: 'span', 'classes': 'underline', exact: true },
+    strikethrough: { inline: 'del' }
+  },
+  resize: false,
+  content_css: [
+    'css/editor.css'
+  ],
+  templates: [
+    {title: 'Box List', description: 'Embedded Etherpad', url: 'templates/etherpad.html'}
+  ]
+});
+
